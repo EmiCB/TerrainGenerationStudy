b12d3949da297096107698022d89ef455392267d
Emi Brown <emicb55@gmail.com>
2019-12-28 13:11:42 -0600

    Episode 3
    Added
    - seeds to generate the same map
    - can scroll around noise map
    - clamped values (mapWidth, mapHeight, lacunarity, octaves, persistance) to avoid errors
    Changed
    - noise map now takes in octaves, persitance, and lacunarity
    - noise scale changes will zoom in and out of center instead of top right corner

--- TerrainGenerationStudy/Assets/Scripts/MapGenerator.cs
+++ TerrainGenerationStudy/Assets/Scripts/MapGenerator.cs
@@ -9,7 +9,15 @@ public class MapGenerator : MonoBehaviour {
     public int mapHeight;
 
     // noise properties
+    public int seed;
     public float noiseScale;
+    public int octaves;
+    [Range(0,1)]
+    public float persistence;
+    public float lacunarity;
+
+    // allows for scrolling around the map
+    public Vector2 offset;
 
     // editor settings
     public bool autoUpdate;
@@ -17,10 +25,19 @@ public class MapGenerator : MonoBehaviour {
     // create noise map and draw onto plane
     public void GenerateMap() {
         // create noise map using Perlin Noise
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
         // display map on plane
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    // called whenever a value is changed
+    private void OnValidate() {
+        // clamp values
+        if (mapWidth < 1) mapWidth = 1;
+        if (mapHeight < 1) mapHeight = 1;
+        if (lacunarity < 1) lacunarity = 1;
+        if (octaves < 0) octaves = 0;
     }
 }
--- TerrainGenerationStudy/Assets/Scripts/Noise.cs
+++ TerrainGenerationStudy/Assets/Scripts/Noise.cs
@@ -6,22 +6,76 @@ using UnityEngine;
 public static class Noise {
 
     // take in values for noise variables and create a 2d array of heights using Perlin Noise
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        // pseudo random number generator that allows seeds to generate the same map every time
+        System.Random prng = new System.Random(seed);
+
+        // offsets to sample each octave from different location
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            // generate offsets based on seed, avoid using numbers too large for prng by using range (-100000, 100000)
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+
+            // store x and y offsets in the array
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         // protect against a divide by zero
         if (scale <= 0) scale = 0.0001f;
 
+        // values to keep track of maximum and minimum noise heights
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        // calculate values for center of the map
+        float halfWidth = mapWidth / 2.0f;
+        float halfHeight = mapHeight / 2.0f;
+
         // loop through noise map and generate height values
         for (int x = 0; x < mapWidth; x++) {
             for (int y = 0; y < mapHeight; y++) {
-                // get height sample point
-                float sampleX = x / scale;
-                float sampleY = y / scale;
+                // create amplitude,frequency, and keep track of current height value
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
 
-                // get Perlin value and apply it to noise map
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+                // loop through all octaves
+                for (int i = 0; i < octaves; i++) {
+                    // get height sample point, subtract center of map to make changing scale zoom in and out of center
+                    float sampleX = (x - halfHeight) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    // get Perlin value in the range [-1, 1]
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    // increase noise height by perlin value of each octave
+                    noiseHeight += perlinValue * amplitude;
+
+                    // change amplitdue and frequency based on persistence and lacunarity at the end of each octave
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                // keep track of maximum and minimum noise heights
+                if (noiseHeight > maxNoiseHeight) {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight) {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                // apply noise height to noise map
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        // loop through noise map
+        for (int x = 0; x < mapWidth; x++) {
+            for (int y = 0; y < mapHeight; y++) {
+                // normalize points by returning value in range [0, 1] based on minimum and maximum heights generated
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
             }
         }
 
